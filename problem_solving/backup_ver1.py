8904c000d5a4a1a7cf9b768de1bd8a2b2a7c5341
Artyom Filimonov <artemforgog@gmail.com>
2022-11-21 21:33:53 +0300

    21.11.2022

--- problem_solving/backup_ver1.py
+++ problem_solving/backup_ver1.py
@@ -17,6 +17,7 @@ target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + 'zip'
 zip_command = "zip -qr {0} {1}".format(target, ' '.join(sourse))
 
 # Запускаем создание резервной копии
+print(zip_command)
 if os.system(zip_command) == 0:
     print('Резервная копия успешно создана в', target)
 else:
